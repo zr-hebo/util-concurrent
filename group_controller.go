8c1bc4020e663f7c94a4b4ae04bb6680a23a097b
hebo <xiaobiao@jd.com>
2018-07-25 17:00:46 +0800

    add group controller

---
+++ group_controller.go
@@ -0,0 +1,32 @@
+package concurrent
+
+// GroupController concurrent group controller
+type GroupController struct {
+	counter chan struct{}
+	maxNum  int
+}
+
+// NewGroupController create a new result collector
+func NewGroupController(num int) (gc *GroupController) {
+	return &GroupController{
+		counter: make(chan struct{}, num),
+		maxNum:  num,
+	}
+}
+
+// Acquire Acquire semaphore
+func (gc *GroupController) Acquire() {
+	gc.counter <- struct{}{}
+}
+
+// Release Release semaphore
+func (gc *GroupController) Release() {
+	<-gc.counter
+}
+
+// WaitEmpty wait all channel empty
+func (gc *GroupController) WaitEmpty() {
+	for i := 0; i < gc.maxNum; i++ {
+		gc.counter <- struct{}{}
+	}
+}
